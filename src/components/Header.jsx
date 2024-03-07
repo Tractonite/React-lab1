12a23153e062f646d111baf6adad9c9ce02ca71a
Midhun Padmanabhan A <mid2002hun@gmail.com>
2024-03-07 18:25:33 +0530

    submiT

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../assests/css/style.css'
+
+export default function Header() {
+    return (
+
+         <header class="header">
+            <div class="wrapper">
+                <h1>HOUSE RENTAL SYSTEM</h1>
+            </div>
+            
+        </header>
+    )
+}
+
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+import RecentlyAdded from '../components/RecentlyAdded'
+export default function MainPage() {
+  return (
+    <React.Fragment>
+        <Header/>
+        <RecentlyAdded/>
+    </React.Fragment>
+  )
+}
